35bfa1cc179f88656eb9a6efcda97a06ba0ce8e5
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2025-07-13 08:30:48 +0530

    first commit

---
+++ day-1/app/font.js
@@ -0,0 +1,15 @@
+import { Montserrat, Oswald } from "next/font/google";
+
+export const fontOne = Montserrat({
+    subsets: ["latin"],
+    display: "swap",
+    weight: "900"
+})
+
+
+export const fonTwo = Oswald({
+
+    subsets: ["latin"],
+    weight: "700",
+    display: "swap"
+})
